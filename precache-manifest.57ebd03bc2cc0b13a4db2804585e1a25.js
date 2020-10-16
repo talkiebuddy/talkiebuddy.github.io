b2f227faf3f9a5257d380e8729e396c7c73369cd
fbtwitter <rzaugusdi354@gmail.com>
2020-10-16 11:24:48 +0700

    Latest Deploy

---
+++ precache-manifest.57ebd03bc2cc0b13a4db2804585e1a25.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31fe1e93adcd0202bc8e",
+    "url": "/css/app.5c43583a.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "e7cc4d7b027529b8f8b7e7f430fe5808",
+    "url": "/index.html"
+  },
+  {
+    "revision": "45355ed585d0ca7ecf5e",
+    "url": "/js/about.94eb3929.js"
+  },
+  {
+    "revision": "31fe1e93adcd0202bc8e",
+    "url": "/js/app.3b92a923.js"
+  },
+  {
+    "revision": "40d0b9d9d2d452fb8542",
+    "url": "/js/chunk-vendors.0219982d.js"
+  },
+  {
+    "revision": "a0a957b2195f09bad990b328d533d813",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
